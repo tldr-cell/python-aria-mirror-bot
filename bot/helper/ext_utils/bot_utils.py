d437270066ec0a4314b62b474f22af0701b24f1d
tldr-cell <tsubarkah@aol.com>
2021-01-03 18:21:06 +0700

    Add encryption on aria, requirements, and monospace of name download

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -92,7 +92,7 @@ def get_readable_message():
     with download_dict_lock:
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<i>{download.name()}</i> - "
+            msg += f"<code>{download.name()}</code> - "
             msg += download.status()
             if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
                 msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code> of " \
